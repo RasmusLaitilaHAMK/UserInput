04e54adfaab8dab97514d277fce55f4916ccc46e
Rasmus <rasmus.laitila@student.hamk.fi>
2023-09-04 09:39:37 +0300

    Kommentinlisäys

--- KäyttäjänSyöte/src/Main.java
+++ KäyttäjänSyöte/src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+    	//Tämä tulee nyt sitten GITHubiin. Hehehehaw
       
        Scanner in = new Scanner(System.in);
       
